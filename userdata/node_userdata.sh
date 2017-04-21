34f51b18407477ccbca0132a02dfd477d7ff9b01
Gilbert Lau <gmflau@gmail.com>
2017-04-21 07:33:11 -0700

    removed old files

--- userdata/node_userdata.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/opc
-
-curl https://raw.githubusercontent.com/DSPN/oracle-bare-metal-cloud-dse/master/extensions/node.sh > node.sh
-
-chmod +x node.sh
-
--- userdata/opscenter_userdata.sh
+++
@@ -1,7 +0,0 @@
-
-curl https://raw.githubusercontent.com/DSPN/oracle-bare-metal-cloud-dse/master/extensions/opscenter.sh > opscenter.sh
-
-chmod +x opscenter.sh
-
-./opscenter.sh bmc
-
