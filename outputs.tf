fa8c0c49d934376e15659b06697f7eefa1f1b14b
Gilbert Lau <gmflau@gmail.com>
2017-07-17 15:38:15 -0700

    Removed OPSC private and public IP addresses

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,5 @@
 # Output the private and public IPs of the DataStax OpsCenter instance
 
-output "DSE_OPSC_PrivateIP" {
-  value = ["${data.baremetal_core_vnic.DSE_OPSC_Vnic.private_ip_address}"]
-}
-
-output "DSE_OPSC_PublicIP" {
-  value = ["${data.baremetal_core_vnic.DSE_OPSC_Vnic.public_ip_address}"]
-}
 
 output "OpsCenter_URL" {
   value = ["${format("%s:8888", data.baremetal_core_vnic.DSE_OPSC_Vnic.public_ip_address)}"]
