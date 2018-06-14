c57221addcd09879deb71a8106f5834776a1b749
Gilbert Lau <gmflau@gmail.com>
2018-06-14 14:40:34 -0700

    Point to 6.0.6 release

--- userdata/lcm_node_userdata.sh
+++ userdata/lcm_node_userdata.sh
@@ -2,7 +2,7 @@
 
 cd ~opc
 
-release="6.0.5"
+release="6.0.6"
 curl https://raw.githubusercontent.com/DSPN/oracle-bmc-terraform-dse/$release/userdata/lcm_node.sh > lcm_node.sh
 
 chmod +x lcm_node.sh
