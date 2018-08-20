94c1735765453400a92c3910bdbd8b22cd440104
Ben Lackey <ben.lackey@outlook.com>
2018-08-20 13:39:36 -0700

    chmod +x

--- env-vars.sh
+++ env-vars.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-# Authentication details
-export TF_VAR_tenancy_ocid="<Tenancy OCID>"
-export TF_VAR_user_ocid="<User OCID>"
-export TF_VAR_fingerprint="<PEM key fingerprint>"
-export TF_VAR_private_key_path="<path to the private key that matches the fingerprint above>"
+# Authentication
+export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaap7ke3udaikcy67jq22k6hzruazzhnsdc3w6ta7al7dziaaddbmcq"
+#export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaazrizli2v5n2rvest5twwpezozdquothjxgzklnvxjuv7tvggapsq"
+#export TF_VAR_fingerprint="<PEM key fingerprint>"
+#export TF_VAR_private_key_path="<path to the private key that matches the fingerprint above>"
 
 # Compartment
-export TF_VAR_compartment_ocid="<compartment OCID>"
+#export TF_VAR_compartment_ocid="<compartment OCID>"
 
-# BMC Region: ex, us-phoenix-1
-export TF_VAR_region="us-phoenix-1"
-export TF_VAR_region="us-ashburn-1"
+# OCI Region
+#export TF_VAR_region="us-ashburn-1"
 
 # Public/private keys used on the instance
-export TF_VAR_ssh_public_key=$(cat /Users/gilbertlau/.ssh/bmc_rsa.pub)
-export TF_VAR_ssh_private_key_path="/Users/gilbertlau/.ssh/bmc_rsa"
-export TF_VAR_ssh_private_key=$(cat /Users/gilbertlau/.ssh/bmc_rsa)
+#export TF_VAR_ssh_public_key=$(cat /Users/gilbertlau/.ssh/bmc_rsa.pub)
+#export TF_VAR_ssh_private_key_path="/Users/gilbertlau/.ssh/bmc_rsa"
+#export TF_VAR_ssh_private_key=$(cat /Users/gilbertlau/.ssh/bmc_rsa)
