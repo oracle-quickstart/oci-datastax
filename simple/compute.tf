7c2ffcbeeea9bb1dfce487c1c9349aebad566f9e
Ben Lackey <ben.lackey@outlook.com>
2018-08-21 16:24:46 -0700

    drop copy paste code

--- simple/compute.tf
+++ simple/compute.tf
@@ -1,5 +1,3 @@
-# Compute resources
-
 resource "oci_core_instance" "DSE_OPSC" {
   provider            = "oci.phx"
   depends_on          = ["oci_core_subnet.DataStax_PublicSubnet_AD_PHX"]
@@ -32,8 +30,6 @@ resource "oci_core_instance" "DSE_OPSC" {
   }
 }
 
-# PHX (Phoenix region)
-
 resource "oci_core_instance" "DSE_Node_PHX_0" {
   provider            = "oci.phx"
   depends_on          = ["oci_core_instance.DSE_OPSC"]
@@ -43,6 +39,7 @@ resource "oci_core_instance" "DSE_Node_PHX_0" {
   image               = "${lookup(data.oci_core_images.OLImageOCID_PHX.images[0], "id")}"
   shape               = "${var.DSE_Shape}"
   subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_PHX.0.id}"
+  count               = "${var.DSE_Cluster_Topology_PHX_Region["AD1_Count"]}"
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
@@ -54,128 +51,4 @@ resource "oci_core_instance" "DSE_Node_PHX_0" {
            "us-phoenix-1"
         ))}"
   }
-
-  count = "${var.DSE_Cluster_Topology_PHX_Region["AD1_Count"]}"
-}
-
-resource "oci_core_instance" "DSE_Node_PHX_1" {
-  provider            = "oci.phx"
-  depends_on          = ["oci_core_instance.DSE_OPSC"]
-  availability_domain = "${lookup(data.oci_identity_availability_domains.PHX_ADs.availability_domains[1],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${format("DSE_AD_PHX_2-%d", count.index)}"
-  image               = "${lookup(data.oci_core_images.OLImageOCID_PHX.images[0], "id")}"
-  shape               = "${var.DSE_Shape}"
-  subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_PHX.1.id}"
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s %s %s %s\n",
-           "./userdata/node.sh",
-           "${data.oci_core_vnic.DSE_OPSC_Vnic.public_ip_address}",
-           "${var.DSE_Cluster_Name}",
-           "us-phoenix-1"
-        ))}"
-  }
-
-  count = "${var.DSE_Cluster_Topology_PHX_Region["AD2_Count"]}"
-}
-
-resource "oci_core_instance" "DSE_Node_PHX_2" {
-  provider            = "oci.phx"
-  depends_on          = ["oci_core_instance.DSE_OPSC"]
-  availability_domain = "${lookup(data.oci_identity_availability_domains.PHX_ADs.availability_domains[2],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${format("DSE_AD_PHX_3-%d", count.index)}"
-  image               = "${lookup(data.oci_core_images.OLImageOCID_PHX.images[0], "id")}"
-  shape               = "${var.DSE_Shape}"
-  subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_PHX.2.id}"
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s %s %s %s\n",
-           "./userdata/node.sh",
-           "${data.oci_core_vnic.DSE_OPSC_Vnic.public_ip_address}",
-           "${var.DSE_Cluster_Name}",
-           "us-phoenix-1"
-        ))}"
-  }
-
-  count = "${var.DSE_Cluster_Topology_PHX_Region["AD3_Count"]}"
-}
-
-# IAD (Ashburn region)
-
-resource "oci_core_instance" "DSE_Node_IAD_0" {
-  provider            = "oci.iad"
-  depends_on          = ["oci_core_instance.DSE_OPSC"]
-  availability_domain = "${lookup(data.oci_identity_availability_domains.IAD_ADs.availability_domains[0],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${format("DSE_AD_IAD_1-%d", count.index)}"
-  image               = "${lookup(data.oci_core_images.OLImageOCID_IAD.images[0], "id")}"
-  shape               = "${var.DSE_Shape}"
-  subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_IAD.0.id}"
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s %s %s %s\n",
-           "./userdata/node.sh",
-           "${data.oci_core_vnic.DSE_OPSC_Vnic.public_ip_address}",
-           "${var.DSE_Cluster_Name}",
-           "us-ashburn-1"
-        ))}"
-  }
-
-  count = "${var.DSE_Cluster_Topology_IAD_Region["AD1_Count"]}"
-}
-
-resource "oci_core_instance" "DSE_Node_IAD_1" {
-  provider            = "oci.iad"
-  depends_on          = ["oci_core_instance.DSE_OPSC"]
-  availability_domain = "${lookup(data.oci_identity_availability_domains.IAD_ADs.availability_domains[1],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${format("DSE_AD_IAD_2-%d", count.index)}"
-  image               = "${lookup(data.oci_core_images.OLImageOCID_IAD.images[0], "id")}"
-  shape               = "${var.DSE_Shape}"
-  subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_IAD.1.id}"
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s %s %s %s\n",
-           "./userdata/node.sh",
-           "${data.oci_core_vnic.DSE_OPSC_Vnic.public_ip_address}",
-           "${var.DSE_Cluster_Name}",
-           "us-ashburn-1"
-        ))}"
-  }
-
-  count = "${var.DSE_Cluster_Topology_IAD_Region["AD2_Count"]}"
-}
-
-resource "oci_core_instance" "DSE_Node_IAD_2" {
-  provider            = "oci.iad"
-  depends_on          = ["oci_core_instance.DSE_OPSC"]
-  availability_domain = "${lookup(data.oci_identity_availability_domains.IAD_ADs.availability_domains[2],"name")}"
-  compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${format("DSE_AD_IAD_3-%d", count.index)}"
-  image               = "${lookup(data.oci_core_images.OLImageOCID_IAD.images[0], "id")}"
-  shape               = "${var.DSE_Shape}"
-  subnet_id           = "${oci_core_subnet.DataStax_PublicSubnet_AD_IAD.2.id}"
-
-  metadata {
-    ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s %s %s %s\n",
-           "./userdata/node.sh",
-           "${data.oci_core_vnic.DSE_OPSC_Vnic.public_ip_address}",
-           "${var.DSE_Cluster_Name}",
-           "us-ashburn-1"
-        ))}"
-  }
-
-  count = "${var.DSE_Cluster_Topology_IAD_Region["AD3_Count"]}"
 }
