a6d50a9a75b13152dcba10970ca21d0b4c37fb53
Ben Lackey <ben.lackey@outlook.com>
2018-08-21 18:21:56 -0700

    reintroduce env-vars

--- simple/variables.tf
+++ simple/variables.tf
@@ -2,16 +2,14 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
+variable "region" {}
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 variable "ssh_private_key" {}
 
-variable "region" {
-  default = "us-ashburn-1"
-}
 
 variable "shape" {
-  default = "VM.Standard1.4"
+  default = "VM.Standard1.2"
 }
 
 variable "nodeCount" {
