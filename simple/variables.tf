68619620ae8926691b04da741a793084fd1be02a
Ben Lackey <ben.lackey@outlook.com>
2018-09-11 15:43:52 -0400

    was using gpu images

--- simple/variables.tf
+++ simple/variables.tf
@@ -35,14 +35,14 @@ variable "dse" {
 # You probably don't need to change these.
 # ---------------------------------------------------------------------------------------------------------------------
 
-// https://docs.cloud.oracle.com/iaas/images/image/c28dc7c4-f901-494c-a72b-b56eafdf0572/
-// Canonical-Ubuntu-16.04-Gen2-GPU-2018.08.15-0
+// https://docs.cloud.oracle.com/iaas/images/image/67026570-4527-42f5-b483-75085379e48c/
+// Canonical-Ubuntu-16.04-2018.08.15-0
 variable "images" {
   type = "map"
   default = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaagofslmfxshjjdvny7vpdbqwmvwxj6zritliwd6k37d5azjljwafq"
-    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaapmjxgjo7zjfmsxy3qalalgrh3sm7aw4u6npzvmtpetsogkyuaisq"
-    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaawwk3dbrs7kadsdbtf7k7prehyzpemmomzoejlcpryazu4ax437kq"
-    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaaked3fo6fy6stwhcwvsvxsn5ul6m3rr2ilfouu2bkwckcmcvbyu3a"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa3tl3phbgdyzjba64h2tgunnwzxbxtw4r36u4ttnsn3m77t6jcurq"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaa7keb3ok2deynxzsz7k5rondhuc7nt5vw6hf3q5xslyiepnqsi3aq"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaafrt5olkogiw2xn74ssu4mjnv7e2wgqkmxwuo4kqihggt74bmgpza"
+    uk-london-1  = "ocid1.image.oc1.uk-london-1.aaaaaaaaizzbceqxadaggbchf6yasjralcqdlh2tmgca6ag5b4gvcg4k4rha"
   }
 }
