2547fd7f3cd745dd82542c8048644b802377cda6
Ben Lackey <ben.lackey@outlook.com>
2018-09-19 13:07:12 -0700

    errrr 6.02

--- scripts/opscenter.sh
+++ scripts/opscenter.sh
@@ -54,7 +54,7 @@ sleep 1m
 --clustername "mycluster" \
 --repouser "datastax@oracle.com" \
 --repopw "*9En9HH4j^p4" \
---dsever "6.5.3" \
+--dsever "6.0.2" \
 --username $username \
 --password $password \
 --dbpasswd $password \
