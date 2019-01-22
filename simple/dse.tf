3fb359898d34e7783abe49c08a8a01a67f8e5fbb
Collin Poczatek <collin.poczatek@gmail.com>
2019-01-22 10:14:23 -0800

    use compartment id, change instance names, add ip outputs

--- simple/dse.tf
+++ simple/dse.tf
@@ -1,6 +1,6 @@
 resource "oci_core_instance" "dse" {
-  display_name        = "dse"
-  compartment_id      = "${var.tenancy_ocid}"
+  display_name        = "dse-${count.index}"
+  compartment_id      = "${var.compartment_ocid}"
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
   shape               = "${var.dse["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
@@ -8,6 +8,12 @@ resource "oci_core_instance" "dse" {
     source_id = "${var.images[var.region]}"
   	source_type = "image"
   }
+
+  create_vnic_details {
+        subnet_id = "${oci_core_subnet.subnet.id}"
+        hostname_label = "dse-${count.index}"
+  }
+
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
     user_data           = "${base64encode(format("%s\n%s\n%s\n",
@@ -18,3 +24,6 @@ resource "oci_core_instance" "dse" {
   }
   count = "${var.dse["node_count"]}"
 }
+
+output "Node public IPs" { value = "${join(",", oci_core_instance.dse.*.public_ip)}" }
+output "Node private IPs" { value = "${join(",", oci_core_instance.dse.*.private_ip)}" }
