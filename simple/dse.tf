c95d11b2503b137a29c644d48e7599f9a6b8a07f
Collin Poczatek <collin.poczatek@gmail.com>
2019-01-29 15:23:00 -0800

    tf fmt

--- simple/dse.tf
+++ simple/dse.tf
@@ -4,26 +4,34 @@ resource "oci_core_instance" "dse" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
   shape               = "${var.dse["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
+
   source_details {
-    source_id = "${var.images[var.region]}"
-  	source_type = "image"
+    source_id   = "${var.images[var.region]}"
+    source_type = "image"
   }
 
   create_vnic_details {
-        subnet_id = "${oci_core_subnet.subnet.id}"
-        hostname_label = "dse-${count.index}"
+    subnet_id      = "${oci_core_subnet.subnet.id}"
+    hostname_label = "dse-${count.index}"
   }
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(join("\n", list(
+
+    user_data = "${base64encode(join("\n", list(
       "#!/usr/bin/env bash",
       "password=${var.dse["password"]}",
       file("../scripts/dse.sh")
     )))}"
   }
+
   count = "${var.dse["node_count"]}"
 }
 
-output "Node public IPs" { value = "${join(",", oci_core_instance.dse.*.public_ip)}" }
-output "Node private IPs" { value = "${join(",", oci_core_instance.dse.*.private_ip)}" }
+output "Node public IPs" {
+  value = "${join(",", oci_core_instance.dse.*.public_ip)}"
+}
+
+output "Node private IPs" {
+  value = "${join(",", oci_core_instance.dse.*.private_ip)}"
+}
--- simple/opscenter.tf
+++ simple/opscenter.tf
@@ -4,13 +4,16 @@ resource "oci_core_instance" "opscenter" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
   shape               = "VM.Standard2.4"
   subnet_id           = "${oci_core_subnet.subnet.id}"
+
   source_details {
-    source_id = "${var.images[var.region]}"
-  	source_type = "image"
+    source_id   = "${var.images[var.region]}"
+    source_type = "image"
   }
+
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-    user_data           = "${base64encode(join("\n", list(
+
+    user_data = "${base64encode(join("\n", list(
       "#!/usr/bin/env bash",
       "username=${var.dse["username"]}",
       "password=${var.dse["password"]}",
@@ -32,5 +35,10 @@ data "oci_core_vnic" "opscenter_vnic" {
   vnic_id = "${lookup(data.oci_core_vnic_attachments.opscenter_vnic_attachments.vnic_attachments[0],"vnic_id")}"
 }
 
-output "OpsCenter URL" { value = "http://${data.oci_core_vnic.opscenter_vnic.public_ip_address}:8888" }
-output "Lifecycle Manager URL" { value = "http://${data.oci_core_vnic.opscenter_vnic.public_ip_address}:8888/opscenter/lcm.html" }
+output "OpsCenter URL" {
+  value = "http://${data.oci_core_vnic.opscenter_vnic.public_ip_address}:8888"
+}
+
+output "Lifecycle Manager URL" {
+  value = "http://${data.oci_core_vnic.opscenter_vnic.public_ip_address}:8888/opscenter/lcm.html"
+}
